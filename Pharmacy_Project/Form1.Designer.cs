dabf7cc9c419c773943a2e0836c0eec8b8ee06e6
Yamen-Bk <bkyamen9@gmail.com>
2026-05-18 15:28:38 +0300

    changing the form name

--- Pharmacy_Project/Form1.Designer.cs
+++ Pharmacy_Project/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Pharmacy_Project
 {
-    partial class Form1
+    partial class loginForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // loginForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(963, 633);
+            Name = "loginForm";
+            Text = "loginForm";
+            ResumeLayout(false);
         }
 
         #endregion
--- Pharmacy_Project/Form1.cs
+++ Pharmacy_Project/Form1.cs
@@ -1,8 +1,8 @@
 namespace Pharmacy_Project
 {
-    public partial class Form1 : Form
+    public partial class loginForm : Form
     {
-        public Form1()
+        public loginForm()
         {
             InitializeComponent();
         }
