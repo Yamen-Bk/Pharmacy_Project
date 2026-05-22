e29b88af51a8a6e6104d4fabf16e77570a4e4b2b
Yamen-Bk <bkyamen9@gmail.com>
2026-05-23 02:29:11 +0300

    yeti login

--- Pharmacy_Project/Form1.Designer.cs
+++ Pharmacy_Project/Form1.Designer.cs
@@ -28,18 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            SignButton = new Guna.UI2.WinForms.Guna2ImageButton();
+            PasswordTextBox = new Guna.UI2.WinForms.Guna2TextBox();
+            UsernameTextBox = new Guna.UI2.WinForms.Guna2TextBox();
+            LoginPic = new Guna.UI2.WinForms.Guna2PictureBox();
+            guna2ShadowPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)LoginPic).BeginInit();
             SuspendLayout();
+            // 
+            // guna2ShadowPanel1
+            // 
+            guna2ShadowPanel1.BackColor = Color.Transparent;
+            guna2ShadowPanel1.Controls.Add(SignButton);
+            guna2ShadowPanel1.Controls.Add(PasswordTextBox);
+            guna2ShadowPanel1.Controls.Add(UsernameTextBox);
+            guna2ShadowPanel1.Controls.Add(LoginPic);
+            guna2ShadowPanel1.FillColor = Color.White;
+            guna2ShadowPanel1.Location = new Point(441, 119);
+            guna2ShadowPanel1.Name = "guna2ShadowPanel1";
+            guna2ShadowPanel1.ShadowColor = Color.Black;
+            guna2ShadowPanel1.Size = new Size(553, 697);
+            guna2ShadowPanel1.TabIndex = 0;
+            // 
+            // SignButton
+            // 
+            SignButton.CheckedState.ImageSize = new Size(64, 64);
+            SignButton.HoverState.ImageSize = new Size(64, 64);
+            SignButton.Image = (Image)resources.GetObject("SignButton.Image");
+            SignButton.ImageOffset = new Point(0, 0);
+            SignButton.ImageRotate = 0F;
+            SignButton.Location = new Point(153, 564);
+            SignButton.Name = "SignButton";
+            SignButton.PressedState.ImageSize = new Size(64, 64);
+            SignButton.ShadowDecoration.CustomizableEdges = customizableEdges1;
+            SignButton.Size = new Size(228, 66);
+            SignButton.TabIndex = 3;
+            SignButton.Click += SignButton_Click;
+            // 
+            // PasswordTextBox
+            // 
+            PasswordTextBox.CustomizableEdges = customizableEdges2;
+            PasswordTextBox.DefaultText = "";
+            PasswordTextBox.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            PasswordTextBox.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            PasswordTextBox.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            PasswordTextBox.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            PasswordTextBox.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            PasswordTextBox.Font = new Font("Segoe UI", 9F);
+            PasswordTextBox.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            PasswordTextBox.Location = new Point(122, 454);
+            PasswordTextBox.Margin = new Padding(4, 5, 4, 5);
+            PasswordTextBox.Name = "PasswordTextBox";
+            PasswordTextBox.PlaceholderText = "";
+            PasswordTextBox.SelectedText = "";
+            PasswordTextBox.ShadowDecoration.CustomizableEdges = customizableEdges3;
+            PasswordTextBox.Size = new Size(303, 54);
+            PasswordTextBox.TabIndex = 2;
+            PasswordTextBox.TextChanged += PasswordTextBox_TextChanged;
+            // 
+            // UsernameTextBox
+            // 
+            UsernameTextBox.CustomizableEdges = customizableEdges4;
+            UsernameTextBox.DefaultText = "";
+            UsernameTextBox.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            UsernameTextBox.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            UsernameTextBox.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            UsernameTextBox.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            UsernameTextBox.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            UsernameTextBox.Font = new Font("Segoe UI", 9F);
+            UsernameTextBox.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            UsernameTextBox.Location = new Point(122, 346);
+            UsernameTextBox.Margin = new Padding(4, 5, 4, 5);
+            UsernameTextBox.Name = "UsernameTextBox";
+            UsernameTextBox.PlaceholderText = "";
+            UsernameTextBox.SelectedText = "";
+            UsernameTextBox.ShadowDecoration.CustomizableEdges = customizableEdges5;
+            UsernameTextBox.Size = new Size(303, 54);
+            UsernameTextBox.TabIndex = 1;
+            UsernameTextBox.TextChanged += UsernameTextBox_TextChanged;
+            // 
+            // LoginPic
+            // 
+            LoginPic.CustomizableEdges = customizableEdges6;
+            LoginPic.Image = (Image)resources.GetObject("LoginPic.Image");
+            LoginPic.ImageRotate = 0F;
+            LoginPic.Location = new Point(181, 72);
+            LoginPic.Name = "LoginPic";
+            LoginPic.ShadowDecoration.CustomizableEdges = customizableEdges7;
+            LoginPic.Size = new Size(186, 185);
+            LoginPic.TabIndex = 0;
+            LoginPic.TabStop = false;
             // 
             // LoginForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1478, 944);
+            Controls.Add(guna2ShadowPanel1);
             Name = "LoginForm";
             Text = "LoginForm";
+            Load += LoginForm_Load;
+            guna2ShadowPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)LoginPic).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel1;
+        private Guna.UI2.WinForms.Guna2PictureBox LoginPic;
+        private Guna.UI2.WinForms.Guna2ImageButton SignButton;
+        private Guna.UI2.WinForms.Guna2TextBox PasswordTextBox;
+        private Guna.UI2.WinForms.Guna2TextBox UsernameTextBox;
     }
 }
--- Pharmacy_Project/Form1.cs
+++ Pharmacy_Project/Form1.cs
@@ -1,10 +1,98 @@
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace Pharmacy_Project
 {
     public partial class LoginForm : Form
     {
+        List<Image> images = new List<Image>();
+        string[] location = new string[25];
         public LoginForm()
         {
             InitializeComponent();
+            location[0] = Application.StartupPath + @"\login_pics\textbox_user_1.jpg";
+            location[1] = Application.StartupPath + @"\login_pics\textbox_user_2.jpg";
+            location[2] = Application.StartupPath + @"\login_pics\textbox_user_3.jpg";
+            location[3] = Application.StartupPath + @"\login_pics\textbox_user_4.jpg";
+            location[4] = Application.StartupPath + @"\login_pics\textbox_user_5.jpg";
+            location[5] = Application.StartupPath + @"\login_pics\textbox_user_6.jpg";
+            location[6] = Application.StartupPath + @"\login_pics\textbox_user_7.jpg";
+            location[7] = Application.StartupPath + @"\login_pics\textbox_user_8.jpg";
+            location[8] = Application.StartupPath + @"\login_pics\textbox_user_9.jpg";
+            location[9] = Application.StartupPath + @"\login_pics\textbox_user_10.jpg";
+            location[10] = Application.StartupPath + @"\login_pics\textbox_user_11.jpg";
+            location[11] = Application.StartupPath + @"\login_pics\textbox_user_12.jpg";
+            location[12] = Application.StartupPath + @"\login_pics\textbox_user_13.jpg";
+            location[13] = Application.StartupPath + @"\login_pics\textbox_user_14.jpg";
+            location[14] = Application.StartupPath + @"\login_pics\textbox_user_15.jpg";
+            location[15] = Application.StartupPath + @"\login_pics\textbox_user_16.jpg";
+            location[16] = Application.StartupPath + @"\login_pics\textbox_user_17.jpg";
+            location[17] = Application.StartupPath + @"\login_pics\textbox_user_18.jpg";
+            location[18] = Application.StartupPath + @"\login_pics\textbox_user_19.jpg";
+            location[19] = Application.StartupPath + @"\login_pics\textbox_user_20.jpg";
+            location[20] = Application.StartupPath + @"\login_pics\textbox_user_21.jpg";
+            location[21] = Application.StartupPath + @"\login_pics\textbox_user_22.jpg";
+            location[22] = Application.StartupPath + @"\login_pics\textbox_user_23.jpg";
+            location[23] = Application.StartupPath + @"\login_pics\textbox_user_24.jpg";
+            LoadImages();
+        }
+        private void LoadImages()
+        {
+
+            for (int i = 0; i < 24; i++)
+            {
+
+                Image img = Image.FromFile(location[i]);
+                images.Add(img);
+            }
+            images.Add(Image.FromFile(Application.StartupPath + @"\login_pics\debut.jpg"));
+
+
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UsernameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (UsernameTextBox.Text.Length > 0 && UsernameTextBox.Text.Length <= 15)
+            {
+                LoginPic.Image = images[UsernameTextBox.Text.Length - 1];
+                LoginPic.BackgroundImageLayout = ImageLayout.Stretch;
+            }
+            else if (UsernameTextBox.Text.Length <= 0)
+                LoginPic.Image = Image.FromFile(Application.StartupPath + @"\login_pics\debut.jpg");
+            else
+                LoginPic.Image = images[23];
+        }
+        private void UsernameTextBox_Click(object sender, EventArgs e)
+        {
+            if (UsernameTextBox.Text.Length > 0)
+                LoginPic.Image = images[UsernameTextBox.Text.Length - 1];
+            else
+                LoginPic.Image = Image.FromFile(Application.StartupPath + @"\login_pics\debut.jpg");
+        }
+
+        private void PasswordTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                LoginPic.Image = Image.FromFile(
+                    Application.StartupPath + @"\login_pics\textbox_password.png"
+                );
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void SignButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
