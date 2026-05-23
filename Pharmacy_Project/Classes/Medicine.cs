d9290b287d0f64fd491503047a7b2ad6e21d40ec
Yamen-Bk <bkyamen9@gmail.com>
2026-05-23 15:02:25 +0300

    add classes and form

---
+++ Pharmacy_Project/Classes/Medicine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy_Project.Classes
+{
+    internal class Medicine
+    {
+    }
+}
